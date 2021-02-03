573e3d61de28864afd21164e42903f95601be481
Mónica Ávila Moreno <moonita@Monicas-MacBook-Pro.local>
2021-02-03 00:15:59 -0600

    foreign keys done

--- migrations/20210128002932-create-rutina.js
+++ migrations/20210128002932-create-rutina.js
@@ -9,7 +9,21 @@ module.exports = {
         type: Sequelize.INTEGER
       },
       id_actividad: {
+        allowNull: false,
+        autoIncrement: true,
         type: Sequelize.INTEGER
+      },
+      actividad: {
+        type: Sequelize.STRING
+      },
+      dia: {
+        type: Sequelize.STRING
+      },
+      hora_inicio: {
+        type: Sequelize.TIME
+      },
+      hota_salida: {
+        type: Sequelize.TIME
       },
       observaciones: {
         type: Sequelize.TEXT
